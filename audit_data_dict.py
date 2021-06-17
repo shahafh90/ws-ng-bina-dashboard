b54f9bd8e63a039ec23598575848420fc36a1722
shahafh <shahafh@rnd-hub.com>
2021-06-17 15:53:53 +0300

    add many historical data

--- audit_data_dict.py
+++ audit_data_dict.py
@@ -1,6 +1,9 @@
 import uuid
 from enum import Enum
 import time
+import random
+
+all_actions = []
 
 
 class ACTION_TYPE_ENUM(Enum):
@@ -19,118 +22,131 @@ class ENTITY_TYPE_ENUM(Enum):
     BinaCase = 'BinaCase'
 
 
-actions = [
-    {
-        "_id": uuid.uuid4(),
-        "doc": {
-            'actionType': ACTION_TYPE_ENUM.EntityCreated.value,
-            'actionTimeStamp': int(time.time() * 1000 - (1000 * 60 * 30)),
-            'actionMetaData': '',
-            'userId': 'wstu15',
-            'userArena': 'זירה א',
-            'userDepartment': 'מדור ג',
-            'userEnv': 'EnvA',
-            'entityType': ENTITY_TYPE_ENUM.BinaInsight.value,
-            'entityId': 'BI12345'
-        }
-    },
-    {
-        "_id": uuid.uuid4(),
-        "doc": {
-            'actionType': ACTION_TYPE_ENUM.UserSearch.value,
-            'actionTimeStamp': int(time.time() * 1000 - (1000 * 60 * 15)),
-            'actionMetaData': 'Al Mabhuh',
-            'userId': 'ssouser3',
-            'userArena': 'זירה ד',
-            'userDepartment': 'מדור א',
-            'userEnv': 'EnvB',
-            'entityType': ENTITY_TYPE_ENUM.BinaNews.value,
-            'entityId': ''
-        }
-    },
-    {
-        "_id": uuid.uuid4(),
-        "doc": {
-            'actionType': ACTION_TYPE_ENUM.UserSearch.value,
-            'actionTimeStamp': int(time.time() * 1000 - (1000 * 60 * 60)),
-            'actionMetaData': 'Al Kayda',
-            'userId': 'ssouser5',
-            'userArena': 'זירה ד',
-            'userDepartment': 'מדור א',
-            'userEnv': 'EnvB',
-            'entityType': '',
-            'entityId': ''
-        }
-    },
-    {
-        "_id": uuid.uuid4(),
-        "doc": {
-            'actionType': ACTION_TYPE_ENUM.EntityCreated.value,
-            'actionTimeStamp': int(time.time() * 1000),
-            'actionMetaData': '',
-            'userId': 'wstu15',
-            'userArena': 'זירה א',
-            'userDepartment': 'מדור ג',
-            'userEnv': 'EnvA',
-            'entityType': ENTITY_TYPE_ENUM.BinaNews.value,
-            'entityId': 'BI12346'
-        }
-    },
-    {
-        "_id": uuid.uuid4(),
-        "doc": {
-            'actionType': ACTION_TYPE_ENUM.EntityCreated.value,
-            'actionTimeStamp': int(time.time() * 1000 - (1000 * 60 * 5)),
-            'actionMetaData': '',
-            'userId': 'wstu15',
-            'userArena': 'זירה ד',
-            'userDepartment': 'מדור ג',
-            'userEnv': 'EnvA',
-            'entityType': ENTITY_TYPE_ENUM.BinaFile.value,
-            'entityId': ''
-        }
-    },
-    {
-        "_id": uuid.uuid4(),
-        "doc": {
-            'actionType': ACTION_TYPE_ENUM.UserLogin.value,
-            'actionTimeStamp': int(time.time() * 1000),
-            'actionMetaData': '',
-            'userId': 'wstu15',
-            'userArena': 'זירה א',
-            'userDepartment': 'מדור ג',
-            'userEnv': 'EnvX',
-            'entityType': '',
-            'entityId': ''
-        }
-    },
-    {
-        "_id": uuid.uuid4(),
-        "doc": {
-            'actionType': ACTION_TYPE_ENUM.UserLogin.value,
-            'actionTimeStamp': int(time.time() * 1000),
-            'actionMetaData': '',
-            'userId': 'ssouser3',
-            'userArena': 'זירה ד',
-            'userDepartment': 'מדור א',
-            'userEnv': 'EnvX',
-            'entityType': '',
-            'entityId': ''
-        }
-    },
-    {
-        "_id": uuid.uuid4(),
-        "doc": {
-            'actionType': ACTION_TYPE_ENUM.UserLogin.value,
-            'actionTimeStamp': int(time.time() * 1000 - (1000 * 60 * 60)),
-            'actionMetaData': '',
-            'userId': 'ssouser5',
-            'userArena': 'זירה ד',
-            'userDepartment': 'מדור א',
-            'userEnv': 'EnvX',
-            'entityType': '',
-            'entityId': ''
-        }
-    }
+class USER_ARENA_ENUM(Enum):
+    ZiraA = 'זירה א'
+    ZiraB = 'זירה ב'
+    ZiraC = 'זירה ג'
+    ZiraD = 'זירה ד'
 
+
+class USER_DEPT_ENUM(Enum):
+    MadorA = 'מדור א'
+    MadorB = 'מדור ב'
+    MadorC = 'מדור ג'
+    MadorD = 'מדור ד'
+
+
+# ################### -------------------- START ENTITY CREATED ACTIONS -------------------- ################### #
+
+data_entity_created_actions = [
+    [
+        {
+            "_id": str(uuid.uuid4()),
+            "doc": {
+                'actionType': ACTION_TYPE_ENUM.EntityCreated.value,
+                'actionTimeStamp': int((time.time() * 1000) - (1000 * 60 * 60 * 24 * days)),
+                'actionMetaData': '',
+                'userId': 'wstu15',
+                'userArena': random.choice(list(USER_ARENA_ENUM)).value,
+                'userDepartment': random.choice(list(USER_DEPT_ENUM)).value,
+                'userEnv': 'EnvA',
+                'entityType': random.choice(list(ENTITY_TYPE_ENUM)).value,
+                'entityId': str(uuid.uuid4())
+            }
+        } for x in range(8)
+    ]
+    for days in range(31)
 ]
+
+actions_entity_created = [item for sublist in data_entity_created_actions for item in sublist]
+all_actions.append(actions_entity_created)
+
+# ################### -------------------- END ENTITY CREATED ACTIONS -------------------- ################### #
+
+# --------------------------------------------------------------------------------------------------------- #
+
+# ################### -------------------- START USER SEARCH ACTIONS -------------------- ################### #
+
+data_user_search_actions = [
+    [
+        {
+            "_id": str(uuid.uuid4()),
+            "doc": {
+                'actionType': ACTION_TYPE_ENUM.UserSearch.value,
+                'actionTimeStamp': int((time.time() * 1000) - (1000 * 60 * 60 * 24 * days)),
+                'actionMetaData': 'Al Kayda',
+                'userId': 'ssouser3',
+                'userArena': random.choice(list(USER_ARENA_ENUM)).value,
+                'userDepartment': random.choice(list(USER_DEPT_ENUM)).value,
+                'userEnv': 'EnvB',
+                'entityType': random.choice(list(ENTITY_TYPE_ENUM)).value,
+                'entityId': ''
+            }
+        } for x in range(4)
+    ]
+    for days in range(31)
+]
+
+actions_user_search = [item for sublist in data_user_search_actions for item in sublist]
+all_actions.append(actions_user_search)
+
+# ################### -------------------- END USER SEARCH ACTIONS -------------------- ################### #
+
+# --------------------------------------------------------------------------------------------------------- #
+
+# ################### -------------------- START USER SEARCH ALL ENTITIES -------------------- ################### #
+data_user_search_all_entities = [
+    [
+        {
+            "_id": str(uuid.uuid4()),
+            "doc": {
+                'actionType': ACTION_TYPE_ENUM.UserSearch.value,
+                'actionTimeStamp': int((time.time() * 1000) - (1000 * 60 * 60 * 24 * days)),
+                'actionMetaData': 'Al Mabhuh',
+                'userId': 'wstu12',
+                'userArena': random.choice(list(USER_ARENA_ENUM)).value,
+                'userDepartment': random.choice(list(USER_DEPT_ENUM)).value,
+                'userEnv': 'EnvB',
+                'entityType': '',
+                'entityId': ''
+            }
+        } for x in range(1)
+    ]
+    for days in range(31)
+]
+
+actions_user_search_all_entities = [item for sublist in data_user_search_all_entities for item in sublist]
+all_actions.append(actions_user_search_all_entities)
+
+# ################### -------------------- END USER SEARCH ALL ENTITIES -------------------- ################### #
+
+# --------------------------------------------------------------------------------------------------------- #
+
+# ################### -------------------- START USER LOGIN ACTIONS -------------------- ################### #
+
+data_user_login_actions = [
+    [
+        {
+            "_id": str(uuid.uuid4()),
+            "doc": {
+                'actionType': ACTION_TYPE_ENUM.UserLogin.value,
+                'actionTimeStamp': int((time.time() * 1000) - (1000 * 60 * 60 * 24 * days)),
+                'actionMetaData': '',
+                'userId': 'ssouser5',
+                'userArena': random.choice(list(USER_ARENA_ENUM)).value,
+                'userDepartment': random.choice(list(USER_DEPT_ENUM)).value,
+                'userEnv': 'EnvX',
+                'entityType': '',
+                'entityId': ''
+            }
+        } for x in range(6)
+    ]
+    for days in range(31)
+]
+
+actions_user_login = [item for sublist in data_user_login_actions for item in sublist]
+all_actions.append(actions_user_login)
+
+# ################### -------------------- END USER LOGIN ACTIONS -------------------- ################### #
+
+# --------------------------------------------------------------------------------------------------------- #
--- create_audit_data.py
+++ create_audit_data.py
@@ -1,33 +1,20 @@
 from elasticsearch import Elasticsearch, helpers
-from audit_data_dict import actions
+from audit_data_dict import all_actions
 
 elastic = Elasticsearch(hosts=['localhost:9200'])
 
 try:
     # make the bulk call, and get a response
-    # response = helpers.bulk(elastic, bulk_json_data("people.json", "employees", "people"))
-    response = helpers.bulk(elastic, actions, index='audit')
-    print("\nRESPONSE:", response)
+    for actions in all_actions:
+        response = helpers.bulk(elastic, actions, index='audit')
+        print("\nRESPONSE:", response)
+
 except Exception as e:
     print("\nERROR:", e)
 
 elastic.indices.refresh(index="audit")
-res = elastic.search(index="audit", body={"query": {"match_all": {}}})
+res = elastic.search(index="audit", body={"query": {"match_all": {}}}, size=1000)
+
 print("Got %d Hits:" % res['hits']['total']['value'])
 for hit in res['hits']['hits']:
-    print(hit['_source'])
-
-# new_event = {
-#
-# }
-# doc_id = uuid.uuid4()
-#
-# res = elastic.index(
-#     index='audit',
-#     id=doc_id,
-#     body=new_event
-# )
-# print(res['result'])
-#
-# res = elastic.get(index="audit", id=doc_id)
-# print(res['_source'])
+    print(hit['_source']['doc'])
